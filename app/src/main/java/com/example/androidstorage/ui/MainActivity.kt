7533fbe51c4acd3e97e86c5968c2e016ecd8ab43
Victor Loveday <56683410+Vicviral@users.noreply.github.com>
2021-09-15 12:05:28 +0100

    Feat: set up photo preview

--- app/src/main/java/com/example/androidstorage/ui/MainActivity.kt
+++ app/src/main/java/com/example/androidstorage/ui/MainActivity.kt
@@ -3,6 +3,9 @@ package com.example.androidstorage.ui
 import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.result.launch
 import com.example.androidstorage.R
 import com.example.androidstorage.databinding.ActivityMainBinding
 import java.io.IOException
@@ -15,6 +18,18 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        val snapPhoto = registerForActivityResult(ActivityResultContracts.TakePicturePreview()) {
+            val isSwitchPrivate = binding.switchPrivate.isChecked
+
+            if (isSwitchPrivate) {
+                //Do something
+            }
+        }
+
+        binding.snapPhoto.setOnClickListener {
+            snapPhoto.launch()
+        }
 
     }
 
